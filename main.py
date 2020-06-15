bb6a8513022d3dc7846f04b697cf3c4b483a98da
0x0u <m0zurillex@gmail.com>
2020-06-15 15:15:55 +0900

    refactor: tweepyからtwythonに移行
    Signed-off-by: 0x0u <m0zurillex@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-import tweepy
 import requests
+from twython import Twython
 
 WEATHER_ICONS = {
     200: "⚡",
@@ -60,39 +60,39 @@ WEATHER_ICONS = {
 
 }
 
+city_id = "1850147"
+user_name = "miya"
 
-class WeatherUserName:
+open_weather_map_api_key = os.environ.get("open_weather_map_key")
+open_weather_map_api = "http://api.openweathermap.org/data/2.5/weather"
 
-    def __init__(self, user_name, city_id):
-        self.OPEN_WEATHER_MAP_API = "http://api.openweathermap.org/data/2.5/weather"
-        self.TWITTER_USER_NAME = user_name
-        self.CITY_ID = city_id
-        self.OPEN_WEATHER_MAP_API_KEY = os.environ.get("OPEN_WEATHER_MAP_API_KEY")
-        self.TWITTER_CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
-        self.TWITTER_CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
-        self.TWITTER_ACCESS_KEY = os.environ.get("TWITTER_ACCESS_KEY")
-        self.TWITTER_ACCESS_SECRET = os.environ.get("TWITTER_ACCESS_SECRET")
-        self.change_twitter_profile()
+twitter_consumer_key = os.environ.get("twitter_consumer_key")
+twitter_consumer_secret = os.environ.get("twitter_consumer_secret")
+twitter_access_key = os.environ.get("twitter_access_key")
+twitter_access_secret = os.environ.get("twitter_access_secret")
 
-    def get_weather_icon(self):
-        query = {
-            "APPID": self.OPEN_WEATHER_MAP_API_KEY,
-            "id": self.CITY_ID,
-            "mode": "json",
-        }
-        r = requests.get(self.OPEN_WEATHER_MAP_API, params=query)
-        weather_id = r.json()["weather"][0]["id"]
-        return WEATHER_ICONS[weather_id]
 
-    def change_twitter_profile(self):
-        weather_icon = self.get_weather_icon()
-        user_name = "{} {}".format(self.TWITTER_USER_NAME, weather_icon)
-        auth = tweepy.OAuthHandler(self.TWITTER_CONSUMER_KEY, self.TWITTER_CONSUMER_SECRET)
-        auth.set_access_token(self.TWITTER_ACCESS_KEY, self.TWITTER_ACCESS_SECRET)
-        api = tweepy.API(auth)
-        api.update_profile(name=user_name)
+def get_weather_icon():
+    query = {
+        "APPID": open_weather_map_api_key,
+        "id": city_id,
+        "mode": "json",
+    }
+    r = requests.get(open_weather_map_api, params=query)
+    weather_id = r.json()["weather"][0]["id"]
+    return WEATHER_ICONS[weather_id]
+
+
+def change_twitter_profile():
+    twitter = Twython(
+        twitter_consumer_key,
+        twitter_consumer_secret,
+        twitter_access_key,
+        twitter_access_secret
+    )
+    weather_user_name = f"{user_name} {get_weather_icon()}"
+    twitter.update_profile(name=weather_user_name)
 
 
 if __name__ == "__main__":
-    WeatherUserName(user_name="miya", city_id=1850147)
-
+    change_twitter_profile()
